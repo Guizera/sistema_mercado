e6c54d7ee3f2972594ce07d494944a71e20ba187
Guilherme Cunha <guizera7@gmail.com>
2018-09-11 19:29:04 -0300

    head mercados.php

--- app/Mercados.php
+++ app/Mercados.php
@@ -6,9 +6,6 @@ use Illuminate\Database\Eloquent\Model;
 
 class Mercados extends Model
 {
-<<<<<<< HEAD
-    //TESTE
-=======
     //tabela de produtos do mercado
 
     protected $table = 'mercados';
@@ -17,5 +14,4 @@ class Mercados extends Model
     //talvez usaria como um tabela pra receber o codigo do mercado para usar como JOIN com tabela de produtos
     //OBS:
     //Ainda estou vendo se esse MODEL vai ser usado no projeto, por via das duvidas deixamos ele aqui em stand-by
->>>>>>> a030d3ab234c7219acb44db6c2fc28a9b3f6c3ed
 }
